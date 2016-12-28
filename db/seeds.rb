41d78bceaf492aa386c31b52ed090bdd6e18b6e0
Kiryl Karetnikau <kiryl_karetnikau@epam.com>
2016-12-28 14:23:11 +0300

    Changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 heroes = [
-     {name: 'Mr. Nice'},
-     {name: 'Narco'},
-     {name: 'Bombasto'},
-     {name: 'Celeritas'},
-     {name: 'Magneta'},
-     {name: 'RubberMan'},
-     {name: 'Dynama'},
-     {name: 'Dr IQ'},
-     {name: 'Magma'},
-     {name: 'Tornado'}
-   ]
+  {name: 'koryushka'},
+  {name: 'Victor'},
+  {name: 'Vitya'},
+  {name: 'Liza'},
+  {name: 'Mr. Nice'},
+  {name: 'Narco'},
+  {name: 'Bombasto'},
+  {name: 'Celeritas'},
+  {name: 'Magneta'},
+  {name: 'RubberMan'},
+  {name: 'Dynama'},
+  {name: 'Dr IQ'},
+  {name: 'Magma'},
+  {name: 'Tornado'}
+]
+5000.times do |i|
+  heroes << {name: "hero_#{i}"}
+end
 Hero.create!(heroes)
